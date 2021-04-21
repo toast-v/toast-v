894c52ec59d680210ea7a1d17f5e35ad3be5926a
liuwy <liuwyhbd@qq.com>
2021-04-20 23:21:09 -0700

    appendFile

--- myHttpServer.js
+++ myHttpServer.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs=require('fs');
 const querystring=require('querystring');
+const url=require('url');
 //var i = 0;
 
 const server = http.createServer((req,res) => {
@@ -36,17 +37,42 @@ const server = http.createServer((req,res) => {
     }
     else if(req.url.slice(0,6)=="/input"){
         res.statusCode = 200;
-        let url1=req.url.split("?");
-
+        let url123=req.url.split("?");
         // let urlquery=url1[1].split("&");
-        
         // let firstQuery=urlquery[0].split("=");
         // let secondQuery=urlquery[1].split("=");
-        let obQuery=querystring.parse(url[1]);
+        let obQuery=querystring.parse(url123[1]);
+        if(obQuery.submit1=="save"){
+            fs.writeFile('./savefile',obQuery.name1,(err)=>{
+                if(err) console.log("Write file error.");
+                else console.log("Write file success.")
+            })
+        }
+
+        else if(obQuery.submit1=="appendSave"){
+            fs.appendFile('./savefile',obQuery.name1,(err)=>{
+                if(err) console.log("Append file error.");
+                else console.log("Append file success.")
+            })
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/html');
+        fs.readFile("form1.html",(err,fsData)=>{
+            if(err){
+                console.log("Read file error-form");
+                throw err;
+            }
+            res.write(fsData);
+            res.end();
+        })
+        //res.write(fsData);
+
+        //console.log(obQuery);
 
         res.setHeader('Content-Type','text/html');
-        res.write(obQuery.name+"<br>");
-        res.write(obQuery.submit+"<br>");
+        res.write(obQuery.name1+"<br>");
+        res.write(obQuery.submit1+"<br>");
 
         res.end("submit success.")
     }
